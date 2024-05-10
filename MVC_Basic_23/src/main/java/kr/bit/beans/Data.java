7f56a6147c797dbd09e32dfb166175884085b361
jeon <wjadbwjd51@gamail.com>
2024-05-10 11:51:23 +0900

    feat: Validator_Customize

---
+++ MVC_Basic_23/src/main/java/kr/bit/beans/Data.java
@@ -0,0 +1,37 @@
+package kr.bit.beans;
+
+import javax.validation.constraints.*;
+
+public class Data {
+
+	@Size(min = 4, max = 10)
+	private String num1;
+
+	private String num2;
+
+	private String num3;
+
+	public String getNum1() {
+		return num1;
+	}
+
+	public void setNum1(String num1) {
+		this.num1 = num1;
+	}
+
+	public String getNum2() {
+		return num2;
+	}
+
+	public void setNum2(String num2) {
+		this.num2 = num2;
+	}
+
+	public String getNum3() {
+		return num3;
+	}
+
+	public void setNum3(String num3) {
+		this.num3 = num3;
+	}
+}
